a5656045bc1e1546a36839f05ed76c4c02720897
PeterRibs <pedrohenriquelr@gmail.com>
2021-12-21 14:18:53 -0300

    update

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://salty-headland-22792.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
